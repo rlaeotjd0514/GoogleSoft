3a2d61b3dbb6540b278aa1c33f6b822a2887988f
대성 김 <dskim0514@naver.com>
2018-12-18 23:26:26 +0900

    First Commit

---
+++ GOOGLESOFT/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace GOOGLESOFT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ServiceMenu = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.HomeButton = new System.Windows.Forms.Button();
+            this.TwitchTVButton = new System.Windows.Forms.Button();
+            this.GoogleStorageButton = new System.Windows.Forms.Button();
+            this.GoogleSheetButton = new System.Windows.Forms.Button();
+            this.GoogleComputeButton = new System.Windows.Forms.Button();
+            this.GmailButton = new System.Windows.Forms.Button();
+            this.GoogleDriveButton = new System.Windows.Forms.Button();
+            this.youtubeAPIButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.OffButton = new System.Windows.Forms.Button();
+            this.SettingButton = new System.Windows.Forms.Button();
+            this.ServiceMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ServiceMenu
+            // 
+            this.ServiceMenu.BackColor = System.Drawing.Color.White;
+            this.ServiceMenu.Controls.Add(this.panel2);
+            this.ServiceMenu.Controls.Add(this.HomeButton);
+            this.ServiceMenu.Controls.Add(this.TwitchTVButton);
+            this.ServiceMenu.Controls.Add(this.GoogleStorageButton);
+            this.ServiceMenu.Controls.Add(this.GoogleSheetButton);
+            this.ServiceMenu.Controls.Add(this.GoogleComputeButton);
+            this.ServiceMenu.Controls.Add(this.GmailButton);
+            this.ServiceMenu.Controls.Add(this.GoogleDriveButton);
+            this.ServiceMenu.Controls.Add(this.youtubeAPIButton);
+            this.ServiceMenu.Location = new System.Drawing.Point(0, 0);
+            this.ServiceMenu.Margin = new System.Windows.Forms.Padding(0);
+            this.ServiceMenu.Name = "ServiceMenu";
+            this.ServiceMenu.Size = new System.Drawing.Size(179, 672);
+            this.ServiceMenu.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Lime;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(20, 81);
+            this.panel2.TabIndex = 1;
+            // 
+            // HomeButton
+            // 
+            this.HomeButton.FlatAppearance.BorderSize = 0;
+            this.HomeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HomeButton.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HomeButton.Image = ((System.Drawing.Image)(resources.GetObject("HomeButton.Image")));
+            this.HomeButton.Location = new System.Drawing.Point(0, 0);
+            this.HomeButton.Margin = new System.Windows.Forms.Padding(0);
+            this.HomeButton.Name = "HomeButton";
+            this.HomeButton.Size = new System.Drawing.Size(179, 81);
+            this.HomeButton.TabIndex = 1;
+            this.HomeButton.Text = "HOME";
+            this.HomeButton.UseVisualStyleBackColor = true;
+            // 
+            // TwitchTVButton
+            // 
+            this.TwitchTVButton.FlatAppearance.BorderSize = 0;
+            this.TwitchTVButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TwitchTVButton.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TwitchTVButton.Image = ((System.Drawing.Image)(resources.GetObject("TwitchTVButton.Image")));
+            this.TwitchTVButton.Location = new System.Drawing.Point(0, 567);
+            this.TwitchTVButton.Margin = new System.Windows.Forms.Padding(0);
+            this.TwitchTVButton.Name = "TwitchTVButton";
+            this.TwitchTVButton.Size = new System.Drawing.Size(179, 105);
+            this.TwitchTVButton.TabIndex = 0;
+            this.TwitchTVButton.Text = "Twitch TV";
+            this.TwitchTVButton.UseVisualStyleBackColor = true;
+            // 
+            // GoogleStorageButton
+            // 
+            this.GoogleStorageButton.FlatAppearance.BorderSize = 0;
+            this.GoogleStorageButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GoogleStorageButton.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GoogleStorageButton.Image = ((System.Drawing.Image)(resources.GetObject("GoogleStorageButton.Image")));
+            this.GoogleStorageButton.Location = new System.Drawing.Point(0, 486);
+            this.GoogleStorageButton.Margin = new System.Windows.Forms.Padding(0);
+            this.GoogleStorageButton.Name = "GoogleStorageButton";
+            this.GoogleStorageButton.Size = new System.Drawing.Size(179, 81);
+            this.GoogleStorageButton.TabIndex = 0;
+            this.GoogleStorageButton.Text = "Google Storage";
+            this.GoogleStorageButton.UseVisualStyleBackColor = true;
+            this.GoogleStorageButton.Click += new System.EventHandler(this.GoogleStorageButton_Click);
+            // 
+            // GoogleSheetButton
+            // 
+            this.GoogleSheetButton.FlatAppearance.BorderSize = 0;
+            this.GoogleSheetButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GoogleSheetButton.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GoogleSheetButton.Image = ((System.Drawing.Image)(resources.GetObject("GoogleSheetButton.Image")));
+            this.GoogleSheetButton.Location = new System.Drawing.Point(0, 405);
+            this.GoogleSheetButton.Margin = new System.Windows.Forms.Padding(0);
+            this.GoogleSheetButton.Name = "GoogleSheetButton";
+            this.GoogleSheetButton.Size = new System.Drawing.Size(179, 81);
+            this.GoogleSheetButton.TabIndex = 0;
+            this.GoogleSheetButton.Text = "Google Sheet";
+            this.GoogleSheetButton.UseVisualStyleBackColor = true;
+            // 
+            // GoogleComputeButton
+            // 
+            this.GoogleComputeButton.FlatAppearance.BorderSize = 0;
+            this.GoogleComputeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GoogleComputeButton.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GoogleComputeButton.Image = ((System.Drawing.Image)(resources.GetObject("GoogleComputeButton.Image")));
+            this.GoogleComputeButton.Location = new System.Drawing.Point(0, 324);
+            this.GoogleComputeButton.Margin = new System.Windows.Forms.Padding(0);
+            this.GoogleComputeButton.Name = "GoogleComputeButton";
+            this.GoogleComputeButton.Size = new System.Drawing.Size(179, 81);
+            this.GoogleComputeButton.TabIndex = 0;
+            this.GoogleComputeButton.Text = "Google Compute";
+            this.GoogleComputeButton.UseVisualStyleBackColor = true;
+            // 
+            // GmailButton
+            // 
+            this.GmailButton.FlatAppearance.BorderSize = 0;
+            this.GmailButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GmailButton.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GmailButton.Image = ((System.Drawing.Image)(resources.GetObject("GmailButton.Image")));
+            this.GmailButton.Location = new System.Drawing.Point(0, 243);
+            this.GmailButton.Margin = new System.Windows.Forms.Padding(0);
+            this.GmailButton.Name = "GmailButton";
+            this.GmailButton.Size = new System.Drawing.Size(179, 81);
+            this.GmailButton.TabIndex = 0;
+            this.GmailButton.Text = "Gmail";
+            this.GmailButton.UseVisualStyleBackColor = true;
+            // 
+            // GoogleDriveButton
+            // 
+            this.GoogleDriveButton.FlatAppearance.BorderSize = 0;
+            this.GoogleDriveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GoogleDriveButton.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GoogleDriveButton.Image = ((System.Drawing.Image)(resources.GetObject("GoogleDriveButton.Image")));
+            this.GoogleDriveButton.Location = new System.Drawing.Point(0, 162);
+            this.GoogleDriveButton.Margin = new System.Windows.Forms.Padding(0);
+            this.GoogleDriveButton.Name = "GoogleDriveButton";
+            this.GoogleDriveButton.Size = new System.Drawing.Size(179, 81);
+            this.GoogleDriveButton.TabIndex = 0;
+            this.GoogleDriveButton.Text = "Google Drive";
+            this.GoogleDriveButton.UseVisualStyleBackColor = true;
+            // 
+            // youtubeAPIButton
+            // 
+            this.youtubeAPIButton.FlatAppearance.BorderSize = 0;
+            this.youtubeAPIButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.youtubeAPIButton.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.youtubeAPIButton.Image = ((System.Drawing.Image)(resources.GetObject("youtubeAPIButton.Image")));
+            this.youtubeAPIButton.Location = new System.Drawing.Point(0, 81);
+            this.youtubeAPIButton.Margin = new System.Windows.Forms.Padding(0);
+            this.youtubeAPIButton.Name = "youtubeAPIButton";
+            this.youtubeAPIButton.Size = new System.Drawing.Size(179, 81);
+            this.youtubeAPIButton.TabIndex = 0;
+            this.youtubeAPIButton.Text = "Youtube API";
+            this.youtubeAPIButton.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(179, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1013, 10);
+            this.panel1.TabIndex = 0;
+            // 
+            // OffButton
+            // 
+            this.OffButton.BackColor = System.Drawing.Color.Gray;
+            this.OffButton.FlatAppearance.BorderSize = 0;
+            this.OffButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OffButton.Location = new System.Drawing.Point(1139, 12);
+            this.OffButton.Name = "OffButton";
+            this.OffButton.Size = new System.Drawing.Size(50, 50);
+            this.OffButton.TabIndex = 1;
+            this.OffButton.UseVisualStyleBackColor = false;
+            // 
+            // SettingButton
+            // 
+            this.SettingButton.FlatAppearance.BorderSize = 0;
+            this.SettingButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SettingButton.Location = new System.Drawing.Point(1083, 12);
+            this.SettingButton.Name = "SettingButton";
+            this.SettingButton.Size = new System.Drawing.Size(50, 50);
+            this.SettingButton.TabIndex = 2;
+            this.SettingButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1192, 675);
+            this.Controls.Add(this.SettingButton);
+            this.Controls.Add(this.OffButton);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ServiceMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "r";
+            this.ServiceMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ServiceMenu;
+        private System.Windows.Forms.Button HomeButton;
+        private System.Windows.Forms.Button TwitchTVButton;
+        private System.Windows.Forms.Button GoogleStorageButton;
+        private System.Windows.Forms.Button GoogleSheetButton;
+        private System.Windows.Forms.Button GoogleComputeButton;
+        private System.Windows.Forms.Button GmailButton;
+        private System.Windows.Forms.Button GoogleDriveButton;
+        private System.Windows.Forms.Button youtubeAPIButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button OffButton;
+        private System.Windows.Forms.Button SettingButton;
+    }
+}
+
